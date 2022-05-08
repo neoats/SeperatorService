37ff4f3d85db7795510281c77392807877932367
fuat atessacan <fuatatessacan@gmail.com>
2022-05-09 00:26:44 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeperatorService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
